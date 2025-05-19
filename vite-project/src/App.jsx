80ae37428b58811e79c3f4828e9272d3bf9e2949
YazidHabibi <yazid2505@student.abudzar.sch.id>
2025-05-19 10:29:13 +0700

    KolabOY

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,9 @@ export default function App() {
         Ini project reaact Abyan aja
       </h1>
       <img src="/public/kuda.png" className="w-1/2 " />
+
+      <h1 className="text-3xl font-bold mt-10">Ini project reaact Yazid aja</h1>
+      <img src="hiu.jpg" alt="" className="border-2 w-1/2"/>
     </div>
   );
 }
