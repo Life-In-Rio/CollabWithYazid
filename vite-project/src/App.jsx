5babb7ee4ad3ad8085683afce46a7819ae3f3747
Life-In-Rio <151702664+Life-In-Rio@users.noreply.github.com>
2025-05-19 10:22:22 +0700

    kolab

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-3xl font-bold underline">
+        Ini project reaact Abyan aja
+      </h1>
+      <img src="/public/kuda.png" className="w-1/2 " />
+    </div>
+  );
+}
